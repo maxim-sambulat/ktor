37263142397dbee0b1d12c45b2e39769c6b1b6a0
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2018-05-22 14:33:16 +0300

    fixup! Add additional type information in receive

--- ktor-client/ktor-client-core/src/io/ktor/client/call/TypeInfo.kt
+++ ktor-client/ktor-client-core/src/io/ktor/client/call/TypeInfo.kt
@@ -5,15 +5,13 @@ import kotlin.reflect.*
 
 class TypeInfo(val type: KClass<*>, val reifiedType: Type)
 
-@Deprecated("INTERNAL API")
-open class TypeBase<T>
+@PublishedApi()
+internal open class TypeBase<T>
 
 inline fun <reified T> typeInfo(): TypeInfo {
-    @Suppress("DEPRECATION")
     val base = object : TypeBase<T>() {}
     val superType = base::class.java.genericSuperclass!!
 
-    @Suppress("UNCHECKED_CAST")
     val reifiedType = (superType as ParameterizedType).actualTypeArguments.first()!!
     return TypeInfo(T::class, reifiedType)
 }
